ca1260d5c8b83e999be0bcceaf0ca75ce90f18b6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 19:26:54 +0000

    added basic JS variables via ID's & Classes

---
+++ app.js
@@ -0,0 +1,38 @@
+//JS test
+console.log ("Hello World");
+//------------------------------------------------------------
+//Ids
+    //Number ids
+    const zeroButton = document.querySelector("#zeroButton");
+    const oneButton = document.querySelector("#oneButton");
+    const twoButton = document.querySelector("#twoButton");
+    const threeButton = document.querySelector("#threeButton");
+    const fourButton = document.querySelector("#fourButton");
+    const fiveButton = document.querySelector("#fiveButton");
+    const sixButton = document.querySelector("#sixButton");
+    const sevenButton = document.querySelector("#sevenButton");
+    const eightButton = document.querySelector("#eightButton");
+    const nineButton = document.querySelector("#nineButton");
+
+    //Operator ids
+    const acButton = document.querySelector("#acButton");
+    const plusMinusButton = document.querySelector("#plusMinusButton");
+    const percentButton = document.querySelector("#percentButton");
+    const divideButton = document.querySelector("#divideButton");
+    const multiplyButton = document.querySelector("#multiplyButton");
+    const minusButton = document.querySelector("#minusButton");
+    const plusButton = document.querySelector("#plusButton");
+    const dotButton = document.querySelector("#dotButton");
+    const equalsButton = document.querySelector("#equalsButton");
+    //Input id
+    const Calc__Top__Input = document.querySelector("#Calc__Top__Input")
+
+//Classes
+    //All buttons
+    const Calc__All__Buttons = document.querySelector(".Calc__All__Buttons");
+
+//--------------------------------------------------------------
+//Functions
+
+
+
