ef4df19f1e420fd7dfe4ebf47e6d625e259ca6db
BuildTools <unconfigured@null.spigotmc.org>
2022-11-20 18:54:17 +0000

    Added minor button functionality to store clicked as variable

--- app.js
+++ app.js
@@ -3,36 +3,124 @@ console.log ("Hello World");
 //------------------------------------------------------------
 //Ids
     //Number ids
-    const zeroButton = document.querySelector("#zeroButton");
-    const oneButton = document.querySelector("#oneButton");
-    const twoButton = document.querySelector("#twoButton");
-    const threeButton = document.querySelector("#threeButton");
-    const fourButton = document.querySelector("#fourButton");
-    const fiveButton = document.querySelector("#fiveButton");
-    const sixButton = document.querySelector("#sixButton");
-    const sevenButton = document.querySelector("#sevenButton");
-    const eightButton = document.querySelector("#eightButton");
-    const nineButton = document.querySelector("#nineButton");
+    let zeroButton = document.querySelector("#zeroButton");
+    let oneButton = document.querySelector("#oneButton");
+    let twoButton = document.querySelector("#twoButton");
+    let threeButton = document.querySelector("#threeButton");
+    let fourButton = document.querySelector("#fourButton");
+    let fiveButton = document.querySelector("#fiveButton");
+    let sixButton = document.querySelector("#sixButton");
+    let sevenButton = document.querySelector("#sevenButton");
+    let eightButton = document.querySelector("#eightButton");
+    let nineButton = document.querySelector("#nineButton");
 
-    //Operator ids
-    const acButton = document.querySelector("#acButton");
-    const plusMinusButton = document.querySelector("#plusMinusButton");
-    const percentButton = document.querySelector("#percentButton");
-    const divideButton = document.querySelector("#divideButton");
-    const multiplyButton = document.querySelector("#multiplyButton");
-    const minusButton = document.querySelector("#minusButton");
-    const plusButton = document.querySelector("#plusButton");
-    const dotButton = document.querySelector("#dotButton");
-    const equalsButton = document.querySelector("#equalsButton");
+//     //Operator ids
+//     const acButton = document.querySelector("#acButton");
+//     const plusMinusButton = document.querySelector("#plusMinusButton");
+//     const percentButton = document.querySelector("#percentButton");
+//     const divideButton = document.querySelector("#divideButton");
+//     const multiplyButton = document.querySelector("#multiplyButton");
+//     const minusButton = document.querySelector("#minusButton");
+//     const plusButton = document.querySelector("#plusButton");
+//     const dotButton = document.querySelector("#dotButton");
+//     const equalsButton = document.querySelector("#equalsButton");
     //Input id
-    const Calc__Top__Input = document.querySelector("#Calc__Top__Input")
+    let Calc__Top__Input = document.querySelector("#Calc__Top__Input")
 
-//Classes
-    //All buttons
-    const Calc__All__Buttons = document.querySelector(".Calc__All__Buttons");
+// //Classes
+//     //All buttons
+//     const Calc__All__Buttons = document.querySelector(".Calc__All__Buttons");
 
 //--------------------------------------------------------------
+
+const zeroButtonClickEvent = () => {
+    zeroButton = "0";
+    Calc__Top__Input += zeroButton;
+    // console.log(zeroButton);
+};
+
+const oneButtonClickEvent = () => {
+    oneButton = "1";
+    // console.log(oneButton);
+};
+
+const twoButtonClickEvent = () => {
+    twoButton = "2";
+    // console.log(twoButton);
+};
+
+const threeButtonClickEvent = () => {
+    threeButton = "3";
+    console.log(threeButton);
+};
+
+const fourButtonClickEvent = () => {
+    fourButton = "4";
+    console.log(fourButton);
+};
+
+const fiveButtonClickEvent = () => {
+    fiveButton = "5";
+    console.log(fiveButton);
+};
+
+const sixButtonClickEvent = () => {
+    sixButton = "6";
+    console.log(sixButton);
+};
+
+const sevenButtonClickEvent = () => {
+    sevenButton = "7";
+    console.log(sevenButton);
+};
+
+const eightButtonClickEvent = () => {
+    eightButton = "8";
+    console.log(eightButton);
+};
+
+const nineButtonClickEvent = () => {
+    nineButton = "9";
+    console.log(nineButton);
+};
+
+
+
 //Functions
+
+if (zeroButtonClickEvent) {
+
+}
+
+const calculation = () => {
+
+};
+
+//Event Listeners
+//Number
+zeroButton.addEventListener("click",zeroButtonClickEvent);
+oneButton.addEventListener("click",oneButtonClickEvent);
+twoButton.addEventListener("click",twoButtonClickEvent);
+threeButton.addEventListener("click",threeButtonClickEvent);
+fourButton.addEventListener("click",fourButtonClickEvent);
+fiveButton.addEventListener("click",fiveButtonClickEvent);
+sixButton.addEventListener("click",sixButtonClickEvent);
+sevenButton.addEventListener("click",sevenButtonClickEvent);
+eightButton.addEventListener("click",eightButtonClickEvent);
+nineButton.addEventListener("click",nineButtonClickEvent);
+// //Operator
+// acButton.addEventListener("click",acButtonClickEvent);
+// plusMinusButton.addEventListener("click",plusMinusButtonClickEvent);
+// percentButton.addEventListener("click",percentButtonClickEvent);
+// divideButton.addEventListener("click",divideButtonClickEvent);
+// multiplyButton.addEventListener("click",multiplyButtonClickEvent);
+// minusButton.addEventListener("click",minusButtonClickEvent);
+// plusButton.addEventListener("click",plusButtonClickEvent);
+// dotButton.addEventListener("click",dotButtonClickEvent);
+// equalsButton.addEventListener("click",equalsButtonClickEvent);
+
+
+
 
 
 
---
+++ pseudocode.md
@@ -0,0 +1,6 @@
+Pseudocode
+
+-Button click stores a number as variable
+-Button click stores an operator as a variable
+-Display variable in text box
+-Equals button does the operation
